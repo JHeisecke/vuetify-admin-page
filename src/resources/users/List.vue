2f470d8e2a543ff9ebdbf9503295da549c684279
Javier Heisecke <javierheiseckee@gmail.com>
2021-02-14 22:34:16 -0300

    Ajusta pantallas de usuarios

--- src/resources/users/List.vue
+++ src/resources/users/List.vue
@@ -1,25 +1,16 @@
 <template>
-  <div>
-    <va-aside-layout :title="asideTitle">
-      <users-show v-if="show" :item="item"></users-show>
-      <users-form v-else :id="id" :item="item" @saved="onSaved"></users-form>
-    </va-aside-layout>
-    <base-material-card :icon="resource.icon" :title="title">
-      <va-list ref="list" disable-create-redirect @action="onAction">
-        <va-data-table
-          :fields="fields"
-          disable-create-redirect
-          disable-show-redirect
-          disable-edit-redirect
-          @item-action="onAction"
-        >
-          <template v-slot:[`field.address`]="{ value }">
-            {{ value.street }} {{ value.suite }} {{ value.city }}
-          </template>
-        </va-data-table>
+  <v-card>
+    <v-card-title>
+      <h1 class="display-1">
+        {{ title }}
+      </h1>
+    </v-card-title>
+    <v-card-text>
+      <va-list>
+        <va-data-table :fields="fields"></va-data-table>
       </va-list>
-    </base-material-card>
-  </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -32,29 +23,12 @@ export default {
         { source: "name", sortable: true },
         { source: "username", sortable: true },
         { source: "email", type: "email" },
-        //{ source: "address", type: "address" }, //custom field
-        "address",
+        { source: "address", type: "address" },
         "phone",
         { source: "website", type: "url" },
         "company.name"
-      ],
-      asideTitle: null,
-      id: null,
-      item: null,
-      show: false
+      ]
     };
-  },
-  methods: {
-    async onAction({ action, title, id, item }) {
-      this.asideTitle = title;
-      this.id = id;
-      this.show = action === "show";
-      this.item = item;
-    },
-    onSaved() {
-      this.asideTitle = null;
-      this.$refs.list.fetchData();
-    }
   }
 };
 </script>
