90c74e239cd6a5dbafab24aa43c83f49388ca46f
Javier Heisecke <javierheiseckee@gmail.com>
2021-02-14 22:49:51 -0300

    Reemplaza el show vcard por defecto por este

---
+++ src/resources/posts/Show.vue
@@ -0,0 +1,27 @@
+<template>
+  <va-show-layout>
+    <va-show :item="item">
+      <v-row justify="center">
+        <v-col sm="4">
+          <base-material-card>
+            <template v-slot:heading>
+              <div class="display-1">
+                {{ title }}
+              </div>
+            </template>
+            <v-card-text>
+              <va-field source="userId"></va-field>
+              <va-field source="body"></va-field>
+            </v-card-text>
+          </base-material-card>
+        </v-col>
+      </v-row>
+    </va-show>
+  </va-show-layout>
+</template>
+
+<script>
+export default {
+  props: ["title", "item"]
+};
+</script>
